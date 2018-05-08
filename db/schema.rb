931d2fee323b6a92efe4c432ece65c0cc953775f
David Crenin <david.crenin@gmail.com>
2018-05-08 12:12:24 +0200

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180504094816) do
+ActiveRecord::Schema.define(version: 20180508100427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20180504094816) do
     t.datetime "updated_at", null: false
     t.string "localisation"
     t.json "images"
+    t.string "photo"
   end
 
 end
