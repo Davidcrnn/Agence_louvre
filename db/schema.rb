269af0b3a17ad1811352c606e8e2a0d6825f4fdc
David Crenin <david.crenin@gmail.com>
2018-05-16 11:07:57 +0200

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180516082227) do
+ActiveRecord::Schema.define(version: 20180516085909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,7 +78,6 @@ ActiveRecord::Schema.define(version: 20180516082227) do
     t.bigint "user_id"
     t.json "images"
     t.boolean "top", default: false, null: false
-    t.boolean "home"
     t.index ["user_id"], name: "index_ventes_on_user_id"
   end
 
