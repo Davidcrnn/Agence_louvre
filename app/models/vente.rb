4153aea94c4af7f985e71e2c2e932bf041c83fe1
David Crenin <david.crenin@gmail.com>
2018-05-30 10:02:48 +0200

    test

--- app/models/vente.rb
+++ app/models/vente.rb
@@ -7,6 +7,7 @@ class Vente < ApplicationRecord
   validates :surface, presence: true
   validates :price, presence: true
   validates :top, inclusion: { in: [ true, false ] }
+  validates :images, presence: true
   has_many :images, :dependent => :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
   mount_uploader :photo, PhotoUploader
