d532d235c634aa62877fac83e4ec3dd86b3b07e1
David Crenin <david.crenin@gmail.com>
2018-05-30 10:25:38 +0200

    ok destroy

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,10 +1,10 @@
 class PicturesController < ApplicationController
-  def detroy
+  def destroy
     # @location = Location.find(params[:id])
     @picture = Picture.find(params[:id])
   @picture.destroy
   redirect_back(fallback_location: root_path)
-  # redirect_to vente_path(@location)
+
 
 end
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -5,8 +5,7 @@ class Location < ApplicationRecord
   validates :surface, presence: true
   validates :price, presence: true
   validates :top, inclusion: { in: [ true, false ] }
-  validates :pictures, presence: true
-  has_many :pictures, dependent: :destroy
+  has_many :pictures, :dependent => :destroy
   accepts_nested_attributes_for :pictures, allow_destroy: true
   mount_uploader :photos, PhotosUploader
 end
