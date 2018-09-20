656697c9106ace37e9be0b2f2dd29b00675b5110
David Crenin <david.crenin@gmail.com>
2018-09-20 12:22:07 +0200

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180920084740) do
+ActiveRecord::Schema.define(version: 20180920065709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,8 +48,6 @@ ActiveRecord::Schema.define(version: 20180920084740) do
     t.bigint "user_id"
     t.boolean "top", default: false, null: false
     t.string "photos"
-    t.boolean "visisble", default: false, null: false
-    t.boolean "visible", default: false, null: false
     t.index ["user_id"], name: "index_locations_on_user_id"
   end
 
@@ -94,7 +92,7 @@ ActiveRecord::Schema.define(version: 20180920084740) do
     t.string "photo"
     t.bigint "user_id"
     t.boolean "top", default: false, null: false
-    t.boolean "visible", default: false, null: false
+    t.boolean "visible"
     t.index ["user_id"], name: "index_ventes_on_user_id"
   end
 
